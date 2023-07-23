f4c2f1279a061b10786dbc4f8824bf7c86207330
pramod choudhary <pramodchoudhary.71@gmail.com>
2023-07-23 13:18:13 +0530

    run bootstrap ui cmd

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('project',[DashboardController::class,'project'] );
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
