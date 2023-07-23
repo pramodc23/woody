27e36a813a3d9714153216753ff29098940209cf
pramod choudhary <pramodchoudhary.71@gmail.com>
2023-07-23 12:04:28 +0530

    change home file with dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,12 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
+class DashboardController extends Controller
 {
     //
     public function index()
     {
-    	return view('home');
+    	return view('dashboard');
     }
 
     public function about()
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,10 +15,10 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/',[HomeController::class,'index'] );
-Route::get('about',[HomeController::class,'about'] );
-Route::get('service',[HomeController::class,'service'] );
-Route::get('project',[HomeController::class,'project'] );
+Route::get('/',[DashboardController::class,'index'] );
+Route::get('about',[DashboardController::class,'about'] );
+Route::get('service',[DashboardController::class,'service'] );
+Route::get('project',[DashboardController::class,'project'] );
 
 
 // Route::get('/', function () {
